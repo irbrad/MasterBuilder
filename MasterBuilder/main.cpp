b69d5d330a317384a2550d2f2daa8d7be703f667
Sammy James <Pawkette@Sammys-MacBook-Pro.local>
2014-12-19 09:06:32 -0500

    Initial Commit

---
+++ MasterBuilder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MasterBuilder
+//
+//  Created by Sammy James on 12/19/14.
+//  Copyright (c) 2014 Pawkette. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
